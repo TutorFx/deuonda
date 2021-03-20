d8a8b4d0be321ba61be8a92f300cb08d86d44cae
TutorFx <gabriel_serejo@live.com>
2021-03-20 16:15:49 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home py-10">
+  <div class="home pt-10">
       <v-container>
         <v-row class="pa-0 mb-10" align-itens="center">
           <v-col class="white--text arte" cols="12" xs="12" sm="12" md="6" xl="8">
@@ -46,26 +46,87 @@
             </v-row>
           </v-col>
         </v-row>
-        <div class="white--text text-center"><small>© DEU ONDA DIGITAL. ALL RIGHTS RESERVED.</small></div>
       </v-container>
+      <footer class="">
+        <div>
+          <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+          <defs>
+          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+          </defs>
+          <g class="parallax">
+          <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+          <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+          <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+          <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+          </g>
+          </svg>
+          <div class="primary--text text-center footer-text"><small>© DEU ONDA DIGITAL. ALL RIGHTS RESERVED.</small></div>
+        </div>
+      </footer>
   </div>
 </template>
 <style lang="scss">
-  .teste{
-
-  }
   .home{
     background-color: #273e68;
     min-height: calc(100vh - 125px);
-  }
-  .onda-cta{
-
   }
   .arte{
     display: flex;
     justify-content: left;
     align-items: center;
   }
+  // waves-
+  .parallax > use {
+  animation: move-forever 25s cubic-bezier(.55,.5,.45,.5)     infinite;
+}
+.parallax > use:nth-child(1) {
+  animation-delay: -2s;
+  animation-duration: 7s;
+}
+.parallax > use:nth-child(2) {
+  animation-delay: -3s;
+  animation-duration: 10s;
+}
+.parallax > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 13s;
+}
+.parallax > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
+}
+@keyframes move-forever {
+  0% {
+   transform: translate3d(-90px,0,0);
+  }
+  100% { 
+    transform: translate3d(85px,0,0);
+  }
+}
+/*Shrinking for mobile*/
+  .waves {
+    height:200px;
+    min-height:200px;
+    width: 100%;
+    transform: translateY(10px);
+  }
+@media (max-width: 768px) {
+  .waves {
+    height:40px;
+    min-height:40px;
+    width: 100%;
+  }
+  .content {
+    height:30vh;
+  }
+  h1 {
+    font-size:24px;
+  }
+}
+.footer-text{
+  background-color: white;
+}
 </style>
 <script>
 // @ is an alias to /src
